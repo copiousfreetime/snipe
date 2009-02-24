7bf30cfb91dc86cf30525131d4998f93dce60aac
Jeremy Hinegardner <jeremy@hinegardner.org>
2009-02-23 20:38:35 -0800

    make bean monitor more expressive

--- example/bean-monitor.rb
+++ example/bean-monitor.rb
@@ -4,31 +4,48 @@ require 'beanstalk-client'
 
 conn = ::Beanstalk::Connection.new( "localhost:11300" )
 
-tube = 'gnip-activity'
+tubes = %w[ gnip-parse gnip-activity ]
 
-samples = 30
+samples    = 10
 sleep_time = 1
-differences = []
+differences = {
+  'gnip-parse'    => [],
+  'gnip-activity' => [],
+}
 
-stats = conn.stats_tube( tube )
-prev_size = stats['current-jobs-ready']
-
+prev_size = {
+  'gnip-parse'   => conn.stats_tube( 'gnip-parse' )['current-jobs-ready'],
+  'gnip-activty' => conn.stats_tube( 'gnip-activity' )['current-jobs-ready']
+}
 
 loop do
   sleep sleep_time
-  stats = conn.stats_tube( tube )
+  puts "-<>-" * 20
 
-  current_size = stats['current-jobs-ready']
-  diff = prev_size - current_size 
-  differences.push diff
-  prev_size = current_size
+  tubes.each do |tube|
 
-  differences.shift if differences.size > samples
+    stats           = conn.stats_tube( tube )
+    current_size    = stats['current-jobs-ready'] || 0
+    t_prev_size     = prev_size[tube] || 0
+    prev_size[tube] = current_size
+    t_diff          = differences[tube]
 
-  sum = differences.inject(0) { |sum, n| sum + n }
-  drain_rate = sum / Float( sleep_time * differences.size ) # jobs / sec
-  estimated_seconds = current_size / drain_rate
-  worker_count = stats['current-jobs-reserved']
-  completed_at = Time.now + estimated_seconds
-  puts "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} (#{tube}) : #{current_size} jobs left draining at #{"%0.2f"% drain_rate} jobs/sec by #{worker_count} workers, estimated completion at #{completed_at.strftime("%Y-%m-%d %H:%M:%S")}"
+    diff            = t_prev_size - current_size 
+    t_diff.push diff
+    t_diff.shift if t_diff.size > samples
+
+    sum             = t_diff.inject(0) { |sum, n| sum + n }
+    sample_seconds  = Float( sleep_time * t_diff.size )
+    drain_rate      = sum / Float( sleep_time * t_diff.size ) # jobs / sec
+    worker_count    = stats['current-jobs-reserved']
+
+    if current_size > 0 then
+      estimated_seconds = current_size / drain_rate
+      completed_at = Time.now + estimated_seconds
+
+      puts "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} (#{tube.center(15)}) : #{current_size} jobs left draining at #{"%0.2f"% drain_rate} jobs/sec by #{worker_count} workers, estimated completion at #{completed_at.strftime("%Y-%m-%d %H:%M:%S")}"
+    else
+      puts "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} (#{tube.center(15)}) : #{current_size} jobs with #{worker_count} workers waiting."
+    end
+  end
 end
