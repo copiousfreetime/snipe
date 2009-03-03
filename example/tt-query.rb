7e3b78d8ff5d970e4ffaa57d730b944049cec53d
Jeremy Hinegardner <jeremy@hinegardner.org>
2009-03-03 11:36:45 -0700

    added tt-query

---
+++ example/tt-query.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'tokyotyrant'
+require 'rubygems'
+require 'hitimes'
+
+include TokyoTyrant
+
+def dump_author_tweets( rdb, author )
+  query = RDBQRY.new( rdb )
+  query.addcond( "author", RDBQRY::QCSTREQ, author )
+  query.setorder( "at", RDBQRY::QOSTRASC )
+  results = query.searchget( %w[ at text ] )
+  puts "#{author} has #{results.size} tweets:"
+  results.each do |r|
+    puts "  #{r['at']} : #{r['text']}"
+  end
+end
+
+rdb = RDBTBL.new
+rdb.open( "playground.copiousfreetime.org", 30303)
+puts "Database has #{rdb.rnum} records"
+author = ARGV.shift
+dump_author_tweets( rdb,  author )
+
