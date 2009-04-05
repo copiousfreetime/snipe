0e8a25319db8aac5e729ae4f3589ae099133c79c
Jeremy Hinegardner <jeremy@hinegardner.org>
2009-04-05 06:37:36 -0600

    try something different

--- example/tt-query.rb
+++ example/tt-query.rb
@@ -9,17 +9,18 @@ include TokyoTyrant
 def dump_author_tweets( rdb, author )
   query = RDBQRY.new( rdb )
   query.addcond( "author", RDBQRY::QCSTREQ, author )
-  query.setorder( "at", RDBQRY::QOSTRASC )
-  results = query.searchget( %w[ at text ] )
+  #query.setorder( "at", RDBQRY::QOSTRASC )
+  results = query.searchget
   puts "#{author} has #{results.size} tweets:"
   results.each do |r|
-    puts "  #{r['at']} : #{r['text']}"
+    #puts r.inspect
+    puts "  #{r[""]} #{r['at']} : #{r['text']}"
   end
 end
 
 rdb = RDBTBL.new
 rdb.open( "playground.copiousfreetime.org", 30303)
 puts "Database has #{rdb.rnum} records"
-author = ARGV.shift
+author = ARGV.shift.strip
 dump_author_tweets( rdb,  author )
 
